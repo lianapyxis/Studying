46e7878d2c2890dd0e8ffb19e16400a3e6098c56
lianapyxis <72408968+lianapyxis@users.noreply.github.com>
2021-01-24 02:05:32 +0500

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -950,3 +950,23 @@ function whatIsInAName(collection, source) {
         
         
 whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+
+
+
+/*
+Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+*/
+
+function spinalCase(str) {
+    // Create a variable for the white space and underscores.
+    var regex = /\s+|_+/g;
+  
+    // Replace low-upper case to low-space-uppercase
+    str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+  
+    // Replace space and underscore with -
+    return str.replace(regex, "-").toLowerCase();
+}
+  
+spinalCase("This Is Spinal Tap");
